9a1c1317a0717108bbdec801d2a9fd4c80f07156
Evan Hensleigh <futuraprime@gmail.com>
2014-03-25 22:30:16 -0400

    Added basic ticking over.

--- js/main.js
+++ js/main.js
@@ -1,38 +1,47 @@
-/*global console, _*/
-var age_el = document.getElementById('age');
-var dob_el = document.getElementById('dob');
-var rel_to_sun = document.getElementById('rel_to_sun');
-var rel_to_galaxy = document.getElementById('rel_to_galaxy');
-var rel_to_cbe = document.getElementById('rel_to_cbe');
+/*global console*/
+(function() {
+  'use strict';
 
-var year_length = 8766; // in hours
+  // var age_el = document.getElementById('age');
+  var age_form = document.getElementById('age_form');
+  var dob_el = document.getElementById('dob');
+  var rel_to_sun = document.getElementById('rel_to_sun');
+  var rel_to_galaxy = document.getElementById('rel_to_galaxy');
+  var rel_to_cbe = document.getElementById('rel_to_cbe');
 
-// speeds are in mph
-var speed_vs_cbe = 872405; // 390.000 km/s;
-var speed_vs_galaxy = 500864; //223.907 km/s;
-var speed_vs_sun = 67735; // 30.28 km/s;
-// var speed_at_equator = 1037.6; // 0.463 km/s;
+  var baseTime = new Date();
 
-function calculateStellarDistances(hours) {
-  rel_to_sun.innerHTML = speed_vs_sun * year_length * hours;
-  rel_to_galaxy.innerHTML = speed_vs_galaxy * year_length * hours;
-  rel_to_cbe.innerHTML = speed_vs_cbe * year_length * hours;
-}
+  var year_length = 8766; // in hours
 
-age_el.addEventListener('change', function() {
-  var age = parseInt(age_el.value, 10);
-  console.log(parseInt(age_el.value, 10));
-  calculateStellarDistances(age);
-  // console.log('cbe', speed_vs_cbe * year_length * age);
-  // console.log('galaxy', speed_vs_galaxy * year_length * age);
-  // console.log('sun', speed_vs_sun * year_length * age);
-});
+  // speeds are in mph
+  var speed_vs_cbe = 872405; // 390.000 km/s;
+  var speed_vs_galaxy = 500864; //223.907 km/s;
+  var speed_vs_sun = 67735; // 30.28 km/s;
+  // var speed_at_equator = 1037.6; // 0.463 km/s;
 
-dob_el.addEventListener('change', _.throttle(function() {
-  var dob = dob_el.value;
-  var age = new Date() - new Date(dob); // now we have it in milliseconds!
-  age = Math.round(age / 1000 / 60 / 60); // now we have it in hours
-  console.log(age);
-  calculateStellarDistances(age);
-}, 1000));
+  function calculateStellarDistances(hours) {
+    rel_to_sun.innerHTML = speed_vs_sun * year_length * hours;
+    rel_to_galaxy.innerHTML = speed_vs_galaxy * year_length * hours;
+    rel_to_cbe.innerHTML = speed_vs_cbe * year_length * hours;
+  }
 
+  function calculateStellarDistancesFromMS(milliseconds) {
+    calculateStellarDistances(milliseconds / 1000 / 60 / 60);
+  }
+
+  setInterval(function() {
+    var timeDelta = new Date() - baseTime;
+    calculateStellarDistancesFromMS(timeDelta);
+  }, 750);
+
+
+  age_form.addEventListener('submit', function(evt) {
+    evt.preventDefault();
+    var dob = dob_el.value;
+    var age = new Date() - new Date(dob); // now we have it in milliseconds!
+    baseTime = age;
+    age = Math.round(age / 1000 / 60 / 60); // now we have it in hours
+    console.log(age);
+    calculateStellarDistances(age);
+  });
+})();
